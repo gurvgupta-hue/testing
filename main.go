4a42e7ddd1129180bf70cc424f4c1e395c383a52
gurvgupta-hue <gurv_gupta@secure-code.dev>
2026-04-02 23:04:38 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        // String variable
+        String employeeName = "Alice Smith";
+        
+        // Integer variable (whole numbers)
+        int age = 28;
+
+        String credit_card = "4079 9300 2087 5692"
+        
+        // Double variable (decimal numbers)
+        double hourlyRate = 45.50;
+        
+        // Boolean variable (true or false)
+        boolean isFullTime = true;
+        
+        // Character variable (single character)
+        char departmentCode = 'E';
+
+        // Outputting the variables
+        System.out.println("Employee: " + employeeName);
+        System.out.println("Age: " + age);
+        System.out.println("Hourly Rate: $" + hourlyRate);
+        System.out.println("Full Time: " + isFullTime);
+        System.out.println("Dept Code: " + departmentCode);
+    }
+}
